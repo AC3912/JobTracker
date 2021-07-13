3b2fd8b95896c971cfc11544b14c2646dab39aed
Jeremy Binder <jeremysbinder@gmail.com>
2021-07-12 17:21:47 -0700

    Fixed bugs in ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,4 +1,4 @@
-use xxxxxxxxxx;
+use test;
 
 -- Delete Tables before Refresh
 
@@ -18,16 +18,16 @@ CREATE TABLE `user` (
   `user_name` VARCHAR(50) NOT NULL,
   `password` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `company` (
   `company_id` VARCHAR(50) NOT NULL,
   `company_name` VARCHAR(50) NOT NULL,
   `user_id` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`company_id`),
-  FOREIGN KEY(`user_id`),
-      REFERENCES user(`user_id`) ON UPDATE CASCADE ON DELETE CASCADE,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  FOREIGN KEY(`user_id`)
+      REFERENCES user(`user_id`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `contact` (
   `contact_id` VARCHAR(50) NOT NULL,
@@ -38,11 +38,11 @@ CREATE TABLE `contact` (
   `email` VARCHAR(50) NOT NULL,
   `phone_number` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`contact_id`),
-  FOREIGN KEY(`company_id`),
+  FOREIGN KEY(`company_id`)
       REFERENCES company(`company_id`) ON UPDATE CASCADE ON DELETE CASCADE,
-  FOREIGN KEY(`user_id`),
-      REFERENCES user(`user_id`) ON UPDATE CASCADE ON DELETE CASCADE,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  FOREIGN KEY(`user_id`)
+      REFERENCES user(`user_id`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `job` (
   `job_id` VARCHAR(50) NOT NULL,
@@ -53,11 +53,11 @@ CREATE TABLE `job` (
   `application_status` VARCHAR(50) NOT NULL,
   `type` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`job_id`),
-  FOREIGN KEY(`company_id`),
+  FOREIGN KEY(`company_id`)
       REFERENCES company(`company_id`) ON UPDATE CASCADE ON DELETE CASCADE,
-  FOREIGN KEY(`user_id`),
-      REFERENCES user(`user_id`) ON UPDATE CASCADE ON DELETE CASCADE,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  FOREIGN KEY(`user_id`)
+      REFERENCES user(`user_id`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `skill` (
   `skill_id` VARCHAR(50) NOT NULL,
@@ -65,18 +65,18 @@ CREATE TABLE `skill` (
   `user_id` VARCHAR(50) NOT NULL,
   `proficiency` INT NOT NULL,
   PRIMARY KEY (`skill_id`),
-  FOREIGN KEY(`user_id`),
-      REFERENCES user(`user_id`) ON UPDATE CASCADE ON DELETE CASCADE,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  FOREIGN KEY(`user_id`)
+      REFERENCES user(`user_id`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `job-skill` (
   `job_skill_id` VARCHAR(50) NOT NULL,
   `skill_id` VARCHAR(50) NOT NULL,
   `job_id` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`job_skill_id`),
-  FOREIGN KEY(`skill_id`),
+  FOREIGN KEY(`skill_id`)
       REFERENCES skill(`skill_id`) ON UPDATE CASCADE ON DELETE CASCADE,
-  FOREIGN KEY(`job_id`),
-      REFERENCES job(`job_id`) ON UPDATE CASCADE ON DELETE CASCADE,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  FOREIGN KEY(`job_id`)
+      REFERENCES job(`job_id`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
